13faaa79ecb9ca14327596ded5cbc95dca206977
yejiniee <02doyoung01@naver.com>
2021-03-24 00:27:25 +0900

    add tapbar

---
+++ app/src/main/java/org/techtown/kakaobottombar/MainActivity.java
@@ -0,0 +1,73 @@
+package org.techtown.kakaobottombar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private Frag1 frag1;
+    private Frag2 frag2;
+    private Frag3 frag3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView=findViewById(R.id.bottomBar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.person:
+                        setFrag(0);
+                        break;
+                    case R.id.chat:
+                        setFrag(1);
+                        break;
+                    case R.id.settings:
+                        setFrag(2);
+                }
+                return true;
+            }
+        });
+        frag1=new Frag1();
+        frag2=new Frag2();
+        frag3=new Frag3();
+        setFrag(0); //첫 프래그먼트 화면을 무엇으로 지정해줄 것인지 선택.
+
+    }
+
+
+    private void setFrag(int n){
+        fm=getSupportFragmentManager();
+        ft=fm.beginTransaction();
+        switch(n){
+            case 0:
+                ft.replace(R.id.main_frame,frag1);
+                ft.commit();
+                break;
+            case 1:
+                ft.replace(R.id.main_frame,frag2);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame,frag3);
+                ft.commit();
+                break;
+
+        }
+
+
+    }
+}
